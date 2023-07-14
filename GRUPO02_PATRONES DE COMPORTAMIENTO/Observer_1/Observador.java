dcc1df0c33dbbe4a9b865acc652e0ee13d347b99
MaricruzFlores2021 <maricruz01062002@gmail.com>
2023-07-13 23:41:35 -0500

    Subiendo patrones de comportamiento

---
+++ GRUPO02_PATRONES DE COMPORTAMIENTO/Observer_1/Observador.java
@@ -0,0 +1,14 @@
+package Observer_1;
+
+public abstract class Observador {
+
+    protected String name;
+    protected Subject sujeto;
+
+
+    public abstract double actualizar();
+    public abstract String moneda();
+
+
+
+}
---
+++ GRUPO02_PATRONES DE COMPORTAMIENTO/Observer_1/SolPE.java
@@ -0,0 +1,21 @@
+package Observer_1;
+
+public class SolPE extends Observador{
+
+    private double valorCambio = 3.67;
+    private String name="Nuevo Sol";
+    public SolPE(Subject sujeto) {
+        this.sujeto = sujeto;
+    }
+
+    @Override
+    public double actualizar() {
+        return sujeto.getEstado() * valorCambio;
+    }
+
+    @Override
+    public String moneda(){
+        return  name;
+    }
+
+}
